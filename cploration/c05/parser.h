2edf1f1d85770b04e490779a090d475fe147d076
Austen Turbyne <austenturbyne@yahoo.com>
2023-11-02 11:24:25 -0700

    c5 done

---
+++ cploration/c05/parser.h
@@ -0,0 +1,22 @@
+/****************************************
+ * C-ploration 5 for CS 271
+ * 
+ * [NAME] $Austen_Turbyne$
+ * [TERM] FALL $2023$
+ * 
+ ****************************************/
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <stdbool.h>
+
+
+#define MAX_LINE_LENGTH  200
+
+/** function prototypes **/
+char *strip(char *s);
+void parse(FILE * fin);
+bool is_Atype(const char *);
+bool is_label(const char *);
+bool is_Ctype(const char *);
